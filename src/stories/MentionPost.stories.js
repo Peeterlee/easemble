15ddbdd2c181df5f0385bd19c3644968a468e892
peterminhlee <peterminhlee@gmail.com>
2020-02-10 11:58:12 -0800

    made stories for components

---
+++ src/stories/MentionPost.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MentionPost from '../comps/MentionPost';
+import '../App.scss';
+
+export default {
+    title:"MentionPost",
+    component:MentionPost
+}
+
+export const DefaultMentionPost = () => {
+    return <MentionPost />
+}
---
+++ src/stories/Sidebar.stories.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Sidebar from '../comps/Sidebar';
+import '../App.scss';
+
+//icons
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import PhotoAlbumOutlinedIcon from '@material-ui/icons/PhotoAlbumOutlined';
+import DateRangeOutlinedIcon from '@material-ui/icons/DateRangeOutlined';
+import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined';
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
+
+export default {
+    title:"Sidebar",
+    component:Sidebar
+}
+
+export const DefaultSidebar = () => {
+    return <Sidebar menus={menus}/>
+}
+
+var menus=[
+    {
+        menu:"Home",
+        icon: <HomeOutlinedIcon style={{fontSize:"16pt", marginRight:"6pt"}} />,
+        submenus:[]
+    },
+    {
+        menu:"Account",
+        icon: <AccountCircleOutlinedIcon style={{fontSize:"16pt", marginRight:"6pt"}} />,
+        submenus:["Profile", "Billing & Payments", "Notifications", "Security", "Logout"]
+    },
+    {
+        menu:"Albums",
+        icon: <PhotoAlbumOutlinedIcon style={{fontSize:"16pt", marginRight:"6pt"}} />,
+        submenus:[]
+    },
+    {
+        menu:"Calender",
+        icon: <DateRangeOutlinedIcon style={{fontSize:"16pt", marginRight:"6pt"}} />,
+        submenus:[]
+    },
+    {
+        menu:"Analytics",
+        icon: <AssessmentOutlinedIcon style={{fontSize:"16pt", marginRight:"6pt"}} />,
+        submenus:[]
+    },
+    {
+        menu:"Settings",
+        icon: <SettingsOutlinedIcon style={{fontSize:"16pt", marginRight:"6pt"}} />,
+        submenus:[]
+    }
+];
+
