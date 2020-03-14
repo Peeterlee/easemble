1c287a573e58100c022cf324e76753af3d06faec
peterminhlee <peterminhlee@gmail.com>
2020-03-13 17:27:47 -0700

    Adjusted styles

--- src/comps/Section/SectionTab/SectionItem/index.js
+++ src/comps/Section/SectionTab/SectionItem/index.js
@@ -11,16 +11,24 @@ function SectionItem({tabTitle, canDelete, active, type }){
     var HashtagClass = "tab-title-hashtag dashboard"; 
     var HashtagClassActive = "tab-title-hashtag tab-title-hashtag-dashboard-active";
     var taggedClass = 'tab-title-hashtag tagged';
-    var taggedClassActive = "tab-title-hashtag tab-title-hashtag-tagged-active"
+    var taggedClassActive = "tab-title-hashtag tab-title-hashtag-tagged-active";
+    var PopupClass = 'tab-title-hashtag popup';
+    var PopupClassActive = '';
     var deleteTagButton = "none";
+    var Xheight = "10px";
             
     if (type === "text"){ tabClass= TextClass } 
     else if (type === "btn"){ tabClass= HashtagClass }
-    else if (type === "tagged"){ tabClass= taggedClass };
+    else if (type === "tagged"){ tabClass= taggedClass }
+    else if(type === 'popup'){ 
+        tabClass= PopupClass 
+        Xheight="7px" 
+    };
 
     if(type === "text" && active){ tabClass= TextClassActive } 
     else if (type === "btn" && active){ tabClass= HashtagClassActive }
-    else if (type === "tagged" && active){ tabClass= taggedClassActive};
+    else if (type === "tagged" && active){ tabClass= taggedClassActive }
+    else if(type === "popup" && active){ tabClass= PopupClassActive };
 
     if (canDelete === "true" ){
         deleteTagButton = ""
@@ -40,7 +48,7 @@ function SectionItem({tabTitle, canDelete, active, type }){
     return(
     <div onClick={()=>{clickTab()}}  className={tabClass}>
         {tabTitle} 
-        <DeleteIcon style={{display:deleteTagButton}} className="deleteIcon"/>
+        <DeleteIcon style={{display:deleteTagButton,height:Xheight}} className="deleteIcon"/>
     </div>
 
     )
